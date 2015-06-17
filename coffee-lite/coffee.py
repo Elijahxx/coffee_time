fd8df855a6d80693adfbeab5905540ece314192b
Thomas Edwards <tom.rb.edwards@gmail.com>
2015-06-17 21:34:08 +0100

    Created coffee-lite, a simple version of the coffee vendor database. Prints types of coffee available

---
+++ coffee-lite/coffee.py
@@ -0,0 +1,13 @@
+class Vendor(object):
+    def __init__(self):
+        self.coffee_types = ['intenso','espresso','lungo','decaffeinato','caramelito']
+
+
+if __name__ == "__main__":
+    vendor = Vendor()
+
+    # Tom wants to buy some coffee. He browses the choices
+    print vendor.coffee_types
+
+
+
